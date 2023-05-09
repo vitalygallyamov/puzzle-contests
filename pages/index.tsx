10608f997dbe24fda5afcdf947ca17287529831c
vr.gallyamov <vr.gallyamov@tensor.ru>
2023-05-09 18:15:47 +0500

    fix index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
-import { Title, Grid, Space } from "@mantine/core";
-
-import Card from '@/components/contests/Card';
+import { Title, Space } from "@mantine/core";
 
 
 export default function Home() {
@@ -8,29 +6,6 @@ export default function Home() {
     <div>
       <Title>Contests</Title>
       <Space h='md'/>
-      <Grid>
-        <Grid.Col span={3}>
-          <Card name="Buy&hold 10 PUZZLE"/>
-        </Grid.Col>
-        <Grid.Col span={3}>
-          <Card name="Buy 1 NFT from market"/>
-        </Grid.Col>
-        <Grid.Col span={3}>
-          <Card name="Stake 10 puzzle"/>
-        </Grid.Col>
-        <Grid.Col span={3}>
-          <Card name="Swap in 5 days"/>
-        </Grid.Col>
-        <Grid.Col span={3}>
-          <Card name="Liquidity to the pool"/>
-        </Grid.Col>
-        <Grid.Col span={3}>
-          <Card name="Supply 100 Waves"/>
-        </Grid.Col>
-        <Grid.Col span={3}>
-          <Card name="Stake 10 Waves"/>
-        </Grid.Col>
-      </Grid>
     </div>
   )
 }
